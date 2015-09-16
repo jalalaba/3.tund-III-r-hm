cd96f09d72bc658a5e42f861a3f9d7fc327716f0
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-16 13:19:09 +0300

    turvalisus ja väljad

--- Page/login.php
+++ Page/login.php
@@ -3,42 +3,70 @@
 	// LOGIN.PHP
 	$email_error = $passw_error = $fname_error = $lname_error = $email2_error = $passw2_error ="";
 	$email = $passw = $fname = $lname ="";
+	//muutujad ab väärtuste jaoks
+	$name="";
 	//echo $_POST["email"];
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		//kontrollin millist nuppu vajutati
+		if(isset($_POST["login"])){
 		//kontrollin et e-post ei ole tühi
-		if (empty($_POST["email"])){
+			if (empty($_POST["email"])){
 			$email_error = "see väli on kohustulik";	
-		}
-		//kontrollin et parool ei ole tühi
-		if (empty($_POST["password"])){
+			}
+			//kontrollin et parool ei ole tühi
+			if (empty($_POST["password"])){
 			$passw_error = "see väli on kohustulik";
-		} else {
+			} else {
 			
-			//kui oleme siia jõudnud, siis parool pole tühi
-			if(strlen($_POST["password"]) < 8){
+				//kui oleme siia jõudnud, siis parool pole tühi
+				if(strlen($_POST["password"]) < 8){
 				$passw_error="peab olema vähemalt 8 tähemärki";
+				}
 			}
 		}
+		
+		elseif((isset($_POST["submit"]))){
 		//kontrollin et eesnimi ei ole tühi
-		if (empty($_POST["first name"])){
+			if (empty($_POST["first name"])){
 			$fname_error = "see väli on kohustulik";
-		}
-		//kontrollin et perekonnanimi ei ole tühi
-		if (empty($_POST["last name"])){
-			$lname_error = "see väli on kohustulik";
-		}	
-		if (empty($_POST["email"])){
-			$email2_error = "see väli on kohustulik";	
-		}
-		if (empty($_POST["password"])){
-			$passw2_error = "see väli on kohustulik";	
-		} else {			
-			if(strlen($_POST["password"]) < 8){
-				$passw2_error="peab olema vähemalt 8 tähemärki";
 			}
+		//kontrollin et perekonnanimi ei ole tühi
+			if (empty($_POST["last name"])){
+			$lname_error = "see väli on kohustulik";
+			}	
+			if (empty($_POST["email2"])){
+			$email2_error = "see väli on kohustulik";			
+			} else {}
+			if (empty($_POST["password2"])){
+			$passw2_error = "see väli on kohustulik";	
+			} else {			
+				if(strlen($_POST["password"]) < 8){
+				$passw2_error="peab olema vähemalt 8 tähemärki";
+				}
+			} else{
+				//kõik korras
+				//test_input eemaldab pahatahlikud osad
+				$name = test_input($_POST['name']);
+			}
+			if($name_error==""){
+				echo "salvestan andmebaasi"
+			}
+			
 		}	
 	}
+function test_input($data) {
+	//võtab ära tühikud,enterid jne
+	$data = trim($data);
+	//võtab ära tagurpidi kaldkriipsud
+	$data = stripslashes($data);
+	//teeb html-i tekstiks
+	$data = htmlspecialchars($data);
+	return $data;
+}
+	
+	
+
 ?>
 <?php
 	$page_title = "Login" ;
@@ -50,13 +78,13 @@
 		<form action="login.php" method="post">
 			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
 			<input name="password" type="password" placeholder="Parool"> <?php echo $passw_error; ?> <br><br>
-			<input type="submit" value="log in"> <br><br>
+			<input name="login" type="submit" value="log in"> <br><br>
 		</form>
 		
 	<h2>Create user</h2>
 	
 		<form action="login.php" method="post">
-			<input name="email2" type="email" placeholder="E-post" <?php echo $email2_error; ?> ><br><br>
+			<input name="email2" type="email" placeholder="E-post" value="<?php echo $email2; ?>" <?php echo $email2_error; ?> ><br><br>
 			<input name="password2" type="password" placeholder="Parool"> <?php echo $passw2_error; ?> <br><br>
 			<input name="first name" type="text" placeholder="Eesnimi"> <?php echo $fname_error; ?><br><br>
 			<input name="last name" type="text" placeholder="Perekonnanimi"> <?php echo $lname_error; ?><br><br>
@@ -65,7 +93,7 @@
 			<input name="sugu" type="radio" value="Mees">Mees<br><br>
 			<input name="vanus" type="number_format" placeholder="Vanus"><br><br>
 			<input name="Linn" type="text" placeholder="Linn"><br><br>
-			<input type="submit" value="Submit"> <br><br>
+			<input name="submit" type="submit" value="Submit"><br><br>
 		</form>	
 	<h2>Minu mvp idee. Lehekülg kus saab hmm...ma ei tea. Kasutajad saavad üles laadida oma joonistusi ja neid müüa. </h2>
 <?php require_once("../footer.php");?>
