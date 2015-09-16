454033ce1e2a09da549ef6ca8b1b0230df76a7a7
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-16 11:30:32 +0300

    asjad

--- footer.php
+++ footer.php
@@ -1,1 +1,3 @@
-Jalus
+<p><i>Lehe tegi Siim, 2015a</p>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,11 @@
-PÄIS
+<html>
+<head>
+	<title>Page title</title>
+</head>
+<body>
+<?php 
+	// Tõstame teise faili sisu siia
+	require_once("menu.php");
+
+
+?>
